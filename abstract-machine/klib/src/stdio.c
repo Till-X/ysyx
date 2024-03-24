842f3dcdb09f75977c2b32fd962fe9d5e45ed229
tracer-ysyx <tracer@ysyx.org>
2024-03-24 14:25:08 +0800

    > compile NEMU ysyx_22040000 张三 Linux banana-Lenovo-XiaoXinPro-14ITL-2021 6.5.0-15-generic #15~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 12 18:54:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:08 up 8 days, 15:59,  1 user,  load average: 1.05, 0.87, 0.76

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,8 +5,55 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+int printf(const char *fmt, ...){
+  char out[100]; //最多输出100个字节
+  va_list ap;
+  va_start(ap, fmt);
+  int i = 0;
+  while(*fmt != '\0'){
+    if(*fmt == '%'){
+      fmt++;
+      switch(*fmt){
+        case 's': {
+          char *string = va_arg(ap, char *);
+          while(*string != '\0'){
+            out[i] = *string;
+            i++;
+            string++;
+          }
+          break;
+        }
+        case 'd': {
+          int d = va_arg(ap, int);
+          int j = 0;
+          char dd[30];
+          while(d / 10){
+            dd[j] = (char)(d % 10 + '0');
+            j++;
+            d = d / 10;
+          }
+          dd[j] = d + '0';
+          for(; j >= 0; j--){
+            out[i] = dd[j];
+            i++;
+          }
+          break;
+        }
+        default: va_end(ap); return -1;
+      }
+    }
+    else{
+      out[i] = *fmt;
+      i++;
+    }
+    fmt++;
+  }
+  va_end(ap);
+  out[i] = '\0';
+  for(i = 0; out[i] != '\0'; i++){
+    putch(out[i]);
+  }
+  return i;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
@@ -14,7 +61,50 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int i = 0;
+  while(*fmt != '\0'){
+    if(*fmt == '%'){
+      fmt++;
+      switch(*fmt){
+        case 's': {
+          char *string = va_arg(ap, char *);
+          while(*string != '\0'){
+            out[i] = *string;
+            i++;
+            string++;
+          }
+          break;
+        }
+        case 'd': {
+          int d = va_arg(ap, int);
+          int j = 0;
+          char dd[30];
+          while(d / 10){
+            dd[j] = (char)(d % 10 + '0');
+            j++;
+            d = d / 10;
+          }
+          dd[j] = d + '0';
+          for(; j >= 0; j--){
+            out[i] = dd[j];
+            i++;
+          }
+          break;
+        }
+        default: va_end(ap); return -1;
+      }
+    }
+    else{
+      out[i] = *fmt;
+      i++;
+    }
+    fmt++;
+  }
+  va_end(ap);
+  out[i] = '\0';
+  return i;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -24,5 +114,4 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
   panic("Not implemented");
 }
-
 #endif
